eaf54263f84bb3381abf2352c14bb03728542aa9
jyotish12 <kjyotish03@gmail.com>
2023-02-03 15:16:41 +0530

    second commit

---
+++ src/test/java/AdminModule/CreateCustomer.java
@@ -0,0 +1,12 @@
+package AdminModule;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class CreateCustomer {
+@Test
+public void createCustomerTest()
+{
+	Reporter.log("created",true);
+}
+}
---
+++ src/test/java/UserModule/DeleteCustomer.java
@@ -0,0 +1,12 @@
+package UserModule;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class DeleteCustomer {
+@Test
+public void deleteCustTest()
+{
+	Reporter.log("deleted successfuly",true);
+}
+}
